96f7583463dadddaefbb08536f8197d372b2aaf8
Yazan Suleiman <Suleiman@summer.iris.washington.edu>
2018-10-18 09:42:26 -0700

    Add 40 blockettes

--- src/main/java/edu/iris/dmc/seed/control/dictionary/B049.java
+++ src/main/java/edu/iris/dmc/seed/control/dictionary/B049.java
@@ -147,22 +147,22 @@ public class B049 extends AbstractDictionaryBlockette implements ResponseBlocket
 		builder.append(this.approximationType);
 		builder.append(this.frequencyUnit);
 
-		builder.append(this.lowerValidFrequencyBound, "#0.00000E00", 12);
-		builder.append(this.upperValidFrequencyBound, "#0.00000E00", 12);
-		builder.append(this.lowerBoundOfApproximation, "#0.00000E00", 12);
-		builder.append(this.upperBoundOfApproximation, "#0.00000E00", 12);
-		builder.append(this.maximumAbsoluteError, "#0.00000E00", 12);
+		builder.append(this.lowerValidFrequencyBound, "-0.00000E-00", 12);
+		builder.append(this.upperValidFrequencyBound, "-0.00000E-00", 12);
+		builder.append(this.lowerBoundOfApproximation, "-0.00000E-00", 12);
+		builder.append(this.upperBoundOfApproximation, "-0.00000E-00", 12);
+		builder.append(this.maximumAbsoluteError, "-0.00000E-00", 12);
 
 		int size = 0;
 		if (this.coefficients != null) {
 			size = this.coefficients.size();
 		}
-		builder.append(size, 4);
+		builder.append(size, 3);
 
 		if (this.coefficients != null) {
 			for (Number coefficient : this.coefficients) {
-				builder.append(coefficient.getValue(), "#0.00000E00", 12);
-				builder.append(coefficient.getError(), "#0.00000E00", 12);
+				builder.append(coefficient.getValue(), "-0.00000E-00", 12);
+				builder.append(coefficient.getError(), "-0.00000E-00", 12);
 			}
 		}
 		builder.replace(3, 7, builder.length(), "####");
