yazan-iris/java-4-seed

commit 366842062a5374e9ca506bfda9b8830e96d12e61

Enhance the java-4-seed test suite.

src/main/java/edu/iris/dmc/io/SeedStringBuilder.java
@@ -233,7 +233,7 @@ public class SeedStringBuilder {
 	}
 
 	public SeedStringBuilder append(double value, String format, int width) {
-		String text = null;
+		String text = "";
 		String pattern = format;
 		boolean signed = false;
 		if (pattern.startsWith("-")) {

src/main/java/edu/iris/dmc/seed/control/dictionary/B030.java
@@ -56,6 +56,9 @@ public class B030 extends AbstractDictionaryBlockette {
 		SeedStringBuilder builder = new SeedStringBuilder(this.getType(), 3).append("####");
 
 		if (this.name != null) {
+			if(this.name.length()>50) {
+				this.name = this.name.substring(0, Math.min(this.name.length(), 50));
+			}
 			builder.append(this.name);
 		}
 		builder.append("~");

src/main/java/edu/iris/dmc/seed/control/dictionary/B031.java
@@ -47,6 +47,9 @@ public class B031 extends AbstractDictionaryBlockette {
 
 		builder.append(this.lookupKey, 4).append(this.classCode);
 		if (this.description != null) {
+			if(this.description.length()>70) {
+				this.description = this.description.substring(0, Math.min(this.description.length(), 70));
+			}
 			builder.append(this.description);
 		}
 		builder.append("~");

src/main/java/edu/iris/dmc/seed/control/dictionary/B032.java
@@ -46,16 +46,25 @@ public class B032 extends AbstractDictionaryBlockette {
 
 		builder.append(this.lookupKey, 2);
 		if (this.titleAuthor != null) {
+			if(this.titleAuthor.length()>70) {
+				this.titleAuthor = this.titleAuthor.substring(0, Math.min(this.titleAuthor.length(), 70));
+			}
 			builder.append(this.titleAuthor);
 		}
 		builder.append("~");
 
 		if (this.publishedCatalog != null) {
+			if(this.publishedCatalog.length()>70) {
+				this.publishedCatalog = this.publishedCatalog.substring(0, Math.min(this.publishedCatalog.length(), 70));
+			}
 			builder.append(this.publishedCatalog);
 		}
 		builder.append("~");
 
 		if (this.publisherName != null) {
+			if(this.publisherName.length()>50) {
+				this.publisherName = this.publisherName.substring(0, Math.min(this.publisherName.length(), 50));
+			}
 			builder.append(this.publisherName);
 		}
 		builder.append("~");

src/main/java/edu/iris/dmc/seed/control/dictionary/B033.java
@@ -30,6 +30,9 @@ public class B033 extends AbstractDictionaryBlockette {
 
 		builder.append(this.lookupKey, 3);
 		if (this.description != null) {
+			if(this.description.length()>50) {
+				this.description = this.description.substring(0, Math.min(this.description.length(), 50));
+			}
 			builder.append(this.description);
 		}
 		builder.append("~");

src/main/java/edu/iris/dmc/seed/control/dictionary/B034.java
@@ -39,10 +39,16 @@ public class B034 extends AbstractDictionaryBlockette {
 
 		builder.append(this.lookupKey, 3);
 		if (this.name != null) {
+			if(this.name.length()>20) {
+				this.name = this.name.substring(0, Math.min(this.name.length(), 20));
+			}
 			builder.append(this.name);
 		}
 		builder.append("~");
 		if (this.description != null) {
+			if(this.description.length()>50) {
+				this.description = this.description.substring(0, Math.min(this.description.length(), 50));
+			}
 			builder.append(this.description);
 		}
 		builder.append("~");

src/main/java/edu/iris/dmc/seed/control/index/B010.java
@@ -101,7 +101,14 @@ public class B010 extends AbstractBlockette implements IndexBlockette {
 		builder.append(this.startTime).append("~");
 		builder.append(this.endTime).append("~");
 		builder.append(this.volumeTime).append("~");
+		
+		if(this.organization.length()>80) {
+			this.organization = this.organization.substring(0, Math.min(this.organization.length(), 80));
+		}
 		builder.append(this.organization).append("~");
+		if(this.label.length()>80) {
+			this.label = this.label.substring(0, Math.min(this.label.length(), 80));
+		}
 		builder.append(this.label).append("~");
 
 		builder.replace(3, 7, builder.length(), "####");

src/main/java/edu/iris/dmc/seed/control/station/B055.java
@@ -99,11 +99,11 @@ public class B055 extends AbstractResponseBlockette {
 		builder.append(this.responses.size(), 4);
 
 		for (Response response : this.responses) {
-			builder.append(response.getFrequency(), "#0.00000E00", 12);
-			builder.append(response.getAmplitude(), "#0.00000E00", 12);
-			builder.append(response.getAmplitudeError(), "#0.00000E00", 12);
-			builder.append(response.getPhaaeAngle(), "#0.00000E00", 12);
-			builder.append(response.getPhaseError(), "#0.00000E00", 12);
+			builder.append(response.getFrequency(), "-0.00000E-00", 12);
+			builder.append(response.getAmplitude(), "-0.00000E-00", 12);
+			builder.append(response.getAmplitudeError(), "-0.00000E-00", 12);
+			builder.append(response.getPhaaeAngle(), "-0.00000E-00", 12);
+			builder.append(response.getPhaseError(), "-0.00000E-00", 12);
 		}
 
 		builder.replace(3, 7, builder.length(), "####");

commit b0a6b06d882c6491554b13bf3b48c8e4280a3cff

Fix seedstringbuilder bug introduced in commit 366842062a5374e9ca506bfda9b8830e96d12e61

pom.xml
@@ -2,7 +2,7 @@
 	<modelVersion>4.0.0</modelVersion>
 	<groupId>edu.iris.dmc</groupId>
 	<artifactId>java-4-seed</artifactId>
-	<version>1.1.0-RC-2-SNAPSHOT</version>
+	<version>1.1.0-RC-1</version>
 	<packaging>jar</packaging>
 
 	<name>java-4-seed</name>

src/main/java/edu/iris/dmc/io/SeedStringBuilder.java
@@ -233,7 +233,7 @@ public class SeedStringBuilder {
 	}
 
 	public SeedStringBuilder append(double value, String format, int width) {
-		String text = "";
+		String text = null;
 		String pattern = format;
 		boolean signed = false;
 		if (pattern.startsWith("-")) {